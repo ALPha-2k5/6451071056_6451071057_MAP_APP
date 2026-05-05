97692f15c5963498e7c3f830d16f2a9734ec46f8
Huy Phatt <phatle219@gmail.com>
2026-05-05 14:00:53 +0700

    feature/wishlist

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,19 @@
+import 'package:thuc_hanh/controller/cart_controller.dart';
+import 'package:thuc_hanh/controller/notification_controller.dart';
+import 'package:thuc_hanh/controller/order_controller.dart';
+import 'package:thuc_hanh/controller/wishlist_controller.dart';
 import 'package:flutter/material.dart';
 import 'app/app.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:get/get.dart';
 import 'controller/login_controller.dart';
-import 'firebase_options.dart';
-
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  Get.put(AuthController());
-  runApp(const MyApp());
+WidgetsFlutterBinding.ensureInitialized();
+await Firebase.initializeApp();
+Get.put(AuthController());
+Get.put(CartController());
+Get.put(OrderController());
+Get.put(WishlistController());
+Get.put(NotificationController());
+runApp(MyApp());
 }
